c32c3f605634b6b5e5d9c81da9f4af8299754c83
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-05-20 10:01:51 -0400

    update

---
+++ src/js/4make/makeInterface.js
@@ -0,0 +1,28 @@
+// makeInterface.js
+
+function makeInterface()
+{
+    let trueAITitle = ce('div');
+    trueAITitle.textConent = 'TRUE AI';
+    ba(trueAITitle);
+
+    ba(ce('hr'));
+
+    let link2026_05_20 = ce('a');
+    link2026_05_20.textContent = 'True AI 2026-05-20';
+    link2026_05_20.href = 'src/files/2026-05-20/001/CATopalian_JavaScript_True_AI.html';
+    link2026_05_20.style.color = 'rgb(170, 170, 170)';
+    link2026_05_20.target = '_blank';
+    link2026_05_20.style.textDecoration = 'none';
+    link2026_05_20.style.fontSize = '25px';
+    ba(link2026_05_20);
+}
+
+//----//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
---
+++ src/js/whenLoaded/whenLoaded.js
@@ -0,0 +1,15 @@
+// whenLoaded.js
+
+function whenLoaded()
+{
+    makeInterface();
+}
+
+//----//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
